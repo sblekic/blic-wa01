93721cb8cf0a218a9095d19abe95d253634a682f
sblekic <blekic.sandi@gmail.com>
2022-04-01 10:20:45 +0200

    vraćeni originalni link umjesto lokalnog servera

--- src/services/index.js
+++ src/services/index.js
@@ -3,8 +3,8 @@ import axios from "axios";
 let Service = {
   async getGlobal(numb) {
     let response = await axios.get(
-      `http://localhost:9999/books.json?_limit=${numb}`
-      //   `https://www.anapioficeandfire.com/api/books?_limit=${numb}`
+      //   `http://localhost:9999/books.json?_limit=${numb}`
+      `https://www.anapioficeandfire.com/api/books?_limit=${numb}`
     );
     // ? -> ternarni operator. ako je status 200 (OK) vrati mi data ako ne vrati error
     return response.status === 200 ? response.data : "No available data";
